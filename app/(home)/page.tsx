efb0af10166ab8fb7f53252c01b5fd51f4289f58
JoaoFelix <joaopaulo_seixas@outlook.com>
2024-02-03 03:14:22 -0300

    feat: add initial search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   const currentDate = new Date();
@@ -15,6 +16,9 @@ export default function Home() {
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Miguel!</h2>
         <p className="capitalize text-sm">{formattedDate}</p>
+      </div>
+      <div className="px-5 mt-6">
+        <Search></Search>
       </div>
     </div>
   );
