cd94927dfc6bbdba6cfcf6cc0f46f05547be5a37
JoaoFelix <joaopaulo_seixas@outlook.com>
2024-02-05 02:54:08 -0300

    feat: add initial booking components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,12 +4,14 @@ import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
 
-export default function Home() {
+export default async function Home() {
   const currentDate = new Date();
   const formattedDate = format(currentDate, "EEEE',' dd 'de' MMMM", {
     locale: ptBR,
   });
+  const barbershops = await db.barbershop.findMany({});
 
   return (
     <div>
@@ -22,8 +24,13 @@ export default function Home() {
         <Search />
       </div>
       <div className="px-5 mt-6">
-        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+          Agendamentos
+        </h2>
         <BookingItem />
+      </div> 
+      <div className="mt-6">
+        <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
       </div>
     </div>
   );
